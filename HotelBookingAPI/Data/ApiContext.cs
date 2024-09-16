1e7f0d92faa458bd9c06ff43861b6fbf48ee5bc9
John Marc Balbada <jmarcbalbada@gmail.com>
2024-09-17 01:52:08 +0800

    <load>[JM] - project initialization

---
+++ HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base (options)
+        {
+            
+        }
+    }
+}
